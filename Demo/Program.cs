ca2dc0ab0064218d9c5428adcb8b9168b957e828
matoo <matoo@SHIKHARPC>
2022-07-07 14:16:03 +0530

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+        public class Program
+        {
+            public static void Main()
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    
+}
